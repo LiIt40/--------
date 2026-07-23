4f175779a785340e3a506142f8a98ac98df5c54f
LiIt40 <vollha233@gmail.com>
2026-07-23 17:48:49 +0300

    исправления

--- app.py
+++ app.py
@@ -63,13 +63,22 @@ df_all.info()
 df_all['order_date'] = pd.to_datetime(df_all['order_date'], errors='coerce')
 df_all['registration_date'] = pd.to_datetime(df_all['registration_date'], errors='coerce')
 
-# Так же видно, что пропусков по столбцам нет, но проверим это еще раз отдельно:
-print(f'Общее количество пропусков:\n{df_all.isnull().sum()}')
+# Напишем функцию проверки пустых значений:
 
-#Проверим наличие дубликатов
 
-print(f'количество дубликатов = {df_all.duplicated().sum()}')
-# Данные очищены и готовы к дальнейшему использованию
+if df_all.isnull().sum().sum() != 0: 
+    print(f'Найдены пропуски в данных: {df_all.isnull().sum()} . Вернитесь и исправьте')
+    st.subheader('Найдены пропуски в данных. Для дальнейшего анализа необходимо их обработать')
+    st.stop()
+
+elif df_all.duplicated().sum() != 0:
+    print(f'В датафрейме найдены дубликаты = {df_all.duplicated().sum()}. Вернитесь и исправьте')
+    st.subheader('Найдены дубликаты строк. Для дальнейшего анализа необходимо их обработать')
+    st.stop()
+else:
+    print ('Данные очищены и готовы к дальнейшему использованию')
+    
+
 
 # 4. Создание дашборда
 
@@ -114,94 +123,104 @@ with original_data:
 
     st.dataframe(df_filtered)
 
+    if df_filtered.empty:
+        st.write('Недостаточно данных для дальнейшего анализа')
+        st.stop()
+    else:
 
 # Расчитаем и выведем в новой вкладке статистики:
 
-with metrics:
+        with metrics:
 
-    total_count_orders= df_filtered['order_id'].count()
-    total_sales = (df_filtered['quantity']*df_filtered['price_per_unit']).sum()
-    unique_users = df_filtered['user_id'].nunique()
-    avg_check = total_sales/total_count_orders
-
-
-    col1,col2 = st.columns([2,2])
-    with col1:
-        st.metric(label='Общее количество заказов', value=total_count_orders)
-        st.metric (label= 'Общая выручка', value=total_sales)
-    with col2:
-        st.metric (label ='Количество уникальных пользователей' , value=unique_users)
-        st.metric (label ='Средний чек' , value=avg_check)
+            total_count_orders= df_filtered['order_id'].count()
+            total_sales = (df_filtered['quantity']*df_filtered['price_per_unit']).sum()
+            unique_users = df_filtered['user_id'].nunique()
+            avg_check = 0 if total_count_orders==0 else total_sales/total_count_orders
+        
 
 
-
-# Визуализация
-
-with visuals:
-
-    fig,(ax1, ax2, ax3) = plt.subplots(3,1, figsize =(10,12))
-
-    # Топ-10 товаров по выручке
-    revenue_by_product= df_filtered.assign(revenue = df_filtered['quantity']
-                                           *df_filtered['price_per_unit']).groupby(
-                                               'item_name')['revenue'].sum().reset_index()
-
-    top_10=revenue_by_product.sort_values('revenue', ascending=False).head(10)
-    
-    # Горизонтальная столбчатая диаграмма 
-    ax1.barh(top_10['item_name'], top_10['revenue'])
-    ax1.set_title('Топ-10 товаров по выручке', loc = 'left')
-    ax1.invert_yaxis()
-
-    # Выручка по категориям товаров
-    revenue_by_category = df_filtered.assign(revenue = df_filtered['quantity']
-                                           *df_filtered['price_per_unit']).groupby(
-                                               'category')['revenue'].sum().reset_index().sort_values('revenue',ascending=False )
-
- 
-    #Круговая диаграмма
-    ax2.pie (revenue_by_category['revenue'], autopct = '%1.1f%%', pctdistance = 0.85)
-    ax2.set_title('Выручка по категориям товаров', loc = 'center')
-    ax2.axis('equal')
-    ax2.legend(
-        revenue_by_category['category'],
-        title='Категории товаров', 
-        loc ='center left',
-        )
-
-    # Зависимость количества заказов от дня недели
-    count_orders_by_day = df_filtered.groupby(df_filtered['order_date']
-                                              .dt.day_name(locale='ru_RU'))['order_id'].count().reset_index()
-    days_ru = ['Понедельник', 'Вторник', 'Среда', 'Четверг' ,'Пятница' ,'Суббота', 'Воскресенье']
-    count_orders_by_day['order_date']=pd.Categorical(count_orders_by_day['order_date'], 
-                                               categories= days_ru,
-                                               ordered=True)
-
-   
-    # Линейный график
-    ax3.plot(
-        count_orders_by_day['order_date'].sort_values(),
-        count_orders_by_day['order_id'],
-        marker = 'o'
-    )
-    ax3.set_title('Зависимость количества заказов от дня недели')
-    ax3.set_xlabel('День недели')
-    ax3.set_ylabel('Количество заказов')
-
-    plt.tight_layout()
-    st.pyplot(fig)
+            col1,col2 = st.columns([2,2])
+            with col1:
+                st.metric(label='Общее количество заказов', value=total_count_orders)
+                st.metric (label= 'Общая выручка', value=total_sales)
+            with col2:
+                st.metric (label ='Количество уникальных пользователей' , value=unique_users)
+                st.metric (label ='Средний чек' , value=avg_check)
 
 
-#Выводы
-with conclusions:
-    st.write(f'Основываясь на данных, полученных в результате анализа можно сделать следующие выводы:\n'
-             f'1. За {(df_all['order_date'].max()-df_all['order_date'].min()).days+1} дней'
-             f'наблюдения было сделано {total_count_orders} заказов. \n'
-             f'2. При этом уникальных пользователей {unique_users}.'
-             ' Следовательно часть пользователей возвращается за повторными покупками.\n'
-             f'3. Основная выручка приходится на категорию: {revenue_by_category.iloc[0,0]}.\n'
-             f'4. Пик продаж приходится на четверг.'
-             ' Так же видим на графике повышение количества продаж в выходные.\n'
-             )
-    
+
+        # Визуализация
+
+        with visuals:
+
+            fig,(ax1, ax2, ax3) = plt.subplots(3,1, figsize =(10,12))
+
+            # Топ-10 товаров по выручке
+            revenue_by_product= df_filtered.assign(revenue = df_filtered['quantity']
+                                                *df_filtered['price_per_unit']).groupby(
+                                                    'item_name')['revenue'].sum().reset_index()
+
+            top_10=revenue_by_product.sort_values('revenue', ascending=False).head(10)
+            
+            # Горизонтальная столбчатая диаграмма 
+            ax1.barh(top_10['item_name'], top_10['revenue'])
+            ax1.set_title('Топ-10 товаров по выручке', loc = 'left')
+            ax1.invert_yaxis()
+
+            # Выручка по категориям товаров
+            revenue_by_category = df_filtered.assign(revenue = df_filtered['quantity']
+                                                *df_filtered['price_per_unit']).groupby(
+                                                    'category')['revenue'].sum().reset_index().sort_values('revenue',ascending=False )
+
+        
+            #Круговая диаграмма
+            if revenue_by_category['revenue'].sum() == 0:
+                ax2.set_title('Нет данных для построения')
+            else:
+            
+                ax2.pie (revenue_by_category['revenue'], autopct = '%1.1f%%', pctdistance = 0.85)
+                ax2.set_title('Выручка по категориям товаров', loc = 'center')
+                ax2.axis('equal')
+                ax2.legend(
+                    revenue_by_category['category'],
+                    title='Категории товаров', 
+                    loc ='center left',
+                    )
+
+            # Зависимость количества заказов от дня недели
+            count_orders_by_day = df_filtered.groupby(df_filtered['order_date']
+                                                    .dt.day_name(locale='ru_RU'))['order_id'].count().reset_index()
+            days_ru = ['Понедельник', 'Вторник', 'Среда', 'Четверг' ,'Пятница' ,'Суббота', 'Воскресенье']
+            count_orders_by_day['order_date']=pd.Categorical(count_orders_by_day['order_date'], 
+                                                    categories= days_ru,
+                                                    ordered=True)
+            sorted_by_date  = count_orders_by_day.sort_values('order_date')
+            # Линейный график
+            ax3.plot(
+                sorted_by_date['order_date'],
+                sorted_by_date['order_id'],
+                marker = 'o'
+            )
+            ax3.set_title('Зависимость количества заказов от дня недели')
+            ax3.set_xlabel('День недели')
+            ax3.set_ylabel('Количество заказов')
+
+            plt.tight_layout()
+            st.pyplot(fig)
+
+
+        #Выводы
+        with conclusions:
+            st.write(f"Основываясь на данных, полученных в результате анализа можно сделать следующие выводы:\n"
+                    f"1. За {(df_all['order_date'].max()-df_all['order_date'].min()).days+1} дней"
+                    f"наблюдения было сделано {total_count_orders} заказов. \n"
+                    f"2. При этом уникальных пользователей {unique_users}."
+                    " Следовательно часть пользователей возвращается за повторными покупками.\n"
+                    f"3. Основная выручка приходится на категорию: {revenue_by_category.iloc[0,0]}.\n"
+                    f"4. Пик продаж приходится на {sorted_by_date.sort_values('order_id', ascending= False).iloc[0,0]}."
+                    f" Так же видим на графике повышение количества продаж в"
+                    f" {sorted_by_date.sort_values('order_id', ascending= False).iloc[1,0]} и "
+                    f"{sorted_by_date.sort_values('order_id', ascending= False).iloc[2,0]}.\n"
+                    )
+            
              
